8bd02e8c60641fb802782e2b931be98ce244d5fb
goszjacek <goszjacek@gmail.com>
2018-12-04 14:23:37 +0100

    haskell lab 5 WIET

---
+++ haskell-lab5/ex1.hs
@@ -0,0 +1,24 @@
+-- co to jest akcja IO?
+-- co to jest funktor?
+-- co to jest funktor aplikatywny?
+-- funkcje z pierszego zadania jak putstr nie sa wazne
+-- notacja do zamiast potokow
+-- operator then
+-- przetlumaczyc do notacji do
+-- na kartktowce nie bedzie plikow ani obslugi wyjatkow
+-- pure umieszcza w kontekscie funktora aplikatywnego
+-- $ raz przenosi do kontestu, potem trzeba *
+
+-- getChar?
+-- putChar?
+-- getline?
+-- print?
+-- czy print dziala na po prostu stworzonych newtypach?
+-- Monad? 
+-- czy hello to bez cudzyslowania to return ? 
+-- jakie typu jest po prostu jakies wczesniej niedeklarowane :t hello ? 
+-- czy zadziala po prostu show hello [nie]
+
+-- definicja algebraicznego typu danych: 
+-- mamy pewne podstawowe typy danych. Na poziomie algebraicznego typu danych mozemy tworzyc nowe typy w oparci o zbiorowa sume lub iloczyn
+
---
+++ haskell-lab5/ex2.hs
@@ -0,0 +1,10 @@
+-- >>
+
+-- to jakiego typu sprowadza sie: ? 
+actUJ  = putChar 'U' >> putChar 'J' >> putChar '\n'
+
+doAct = do
+    putChar 'U'
+    putChar 'J'
+    putChar '\n'
+
